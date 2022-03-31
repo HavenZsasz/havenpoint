b96a99786cb80602453c59321c8d86c0ff0cb64f
iPsXwhite <timko999@icloud.com>
2022-03-31 16:03:56 +0200

    Update firstJS.js

--- multipage/firstJS.js
+++ multipage/firstJS.js
@@ -20,3 +20,9 @@ document.write(" "+ ftest.toFixed(2));*/
 
 "ok".charAt(0); //= o
 "ok".charAt(1); //= 1
+
+console.log("beispiel".indexOf("e")); //=1; zählt von vorn nach hinten
+console.log("beispiel".lastIndexOf("e")); //=6; zählt von hinten nach vorn, zeigt aber die Stelle von vorne gesehen an, sprich das hinterste e von vorn gesehen
+console.log("beispiel".indexOf("e", 2)); //=6 zählt zum 2. e
+console.log("beispiel".lastIndexOf("e", 5)); //=1; zählt zum 2. e von hinten gesehen, aber zeigt Stelle von vorn gesehen an
+console.log("beispiel".indexOf("katze")); //=-1; wenn der Suchstring gar nicht vorkommt, ist das Ergebnis -1
