c54535eeaa6eefd37c7f7a22355c7576143e218a
iPsXwhite <timko999@icloud.com>
2022-04-04 14:07:02 +0200

    Create ttestJS.js

---
+++ multipage/ttestJS.js
@@ -0,0 +1,22 @@
+
+
+
+function durchschnitt(zahl1, zahl2, zahl3) {
+    
+    return (zahl1+zahl2+zahl3) /3;
+    
+
+}
+
+console.log(durchschnitt(3, 4, 6));
+
+var array = [5, 4, 3, 2,1 ];
+var anderesArray = [1,2,3,4,5];
+array.reverse();
+for(var i=0; i<5; i++) {
+console.log(array[i])
+}
+var combine = array.concat(anderesArray);
+for(i=0; i<10;i++) {
+    console.log(combine[i]);
+}
