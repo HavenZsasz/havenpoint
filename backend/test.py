84c07a09e2c12fd19ea8312d0f8fc75f1760c956
iPsXwhite <timko999@icloud.com>
2022-05-21 18:31:09 +0200

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -1,3 +1,8 @@
 eingabe = input("Dein Name?")
 if eingabe == "Tim":
     print("Guten Tag")
+    mein_text = "Hallo Welt"
+    print(mein_text)
+    erstes_wort, zweites_wort = "Hallo", "Welt"
+    ergebnis = erstes_wort + ", " + zweites_wort
+    print(ergebnis)
