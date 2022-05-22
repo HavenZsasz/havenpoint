0cbe3c6bfae1b4f4bddbd2cb5a353b915965aaa2
iPsXwhite <timko999@icloud.com>
2022-05-22 17:32:07 +0200

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -7,8 +7,17 @@ if eingabe == "Tim":
     ergebnis = erstes_wort + ", " + zweites_wort
     print(ergebnis)
     print(erstes_wort, zweites_wort, sep='*') #sep legt fest, wie die Worte getrennt werden -> default = LEERZEICHEN
-    print(erstes_wort, end=" ")
+    print(erstes_wort, end=" ")#end steuert Zeielnumbruch, default-Wert: "\n"
     print(zweites_wort)
     import sys
     print(sys.version_info)
+    eggs = None
+    spam = None
+
+    print(eggs)
+    multiString = "MultiString " * 10 #Strings sind hier tatsächlich mutliplizierbar (nur per Integer möglich
+    
+    print(multiString)
+    #typecasting:
+    typeCasting = int("42") + 42 #float() #str()
     
