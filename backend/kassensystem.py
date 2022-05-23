8a79ea3ebe5c172f852b0303e24847bc0833c99d
iPsXwhite <timko999@icloud.com>
2022-05-23 15:03:28 +0200

    .

---
+++ backend/kassensystem.py
@@ -0,0 +1,4 @@
+eingabePreis = float(input("Preis eintragen:"))
+eingabeBezahlt = float(input("Vom Kunden bekommenes Geld:"))
+rueckgeld = eingabeBezahlt - eingabePreis
+print("Das Rückgeld beträgt:", rueckgeld)
