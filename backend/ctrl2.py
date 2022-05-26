b6d67ddfc0dad8e5f22b393050853c65273e13a0
iPsXwhite <timko999@icloud.com>
2022-05-26 22:03:27 +0200

    Update ctrl2.py

--- backend/ctrl2.py
+++ backend/ctrl2.py
@@ -3,10 +3,18 @@ weiter = "j"
 while weiter == "j" or weiter == "ja" or weiter == "y" :
     preis = float(input("Der Preis: "))
     bezahlt = float(input("Bezahlt: "))
-    if(bezahlt < preis) {
-        print("Der gezahlte Betrag ist zu gering. Bitte")
-    }
-    rueckgeld = bezahlt - preis
-    print("Das Rückgeld beträgt: ", rueckgeld)
-    weiter = input("Weitere Eingabe?  ")
+    if bezahlt < preis: 
+        print("Der gezahlte Betrag ist zu gering. Bitte zahlen!")
+    elif bezahlt == preis:
+        print("Preis übereinstimmt genau mit der gezahlten Summe.")
+        weiter = input("Weitere Eingabe?  ")
+    else:
+        rueckgeld = bezahlt - preis
+        print("Das Rückgeld beträgt: ", rueckgeld)
+        weiter = input("Weitere Eingabe?  ")        
+        
+    
+    
+    
+    
 print("Kasse geschlossen")
