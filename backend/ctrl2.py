68ba4d860267c0ab3e8f6ec44f5b47626cfa3d2b
iPsXwhite <timko999@icloud.com>
2022-05-26 17:41:38 +0200

    Update ctrl2.py

--- backend/ctrl2.py
+++ backend/ctrl2.py
@@ -1,9 +1,9 @@
 weiter = "j"
 
-while weiter == "j":
+while weiter == "j" or weiter == "ja" or weiter == "y" :
     preis = float(input("Der Preis: "))
     bezahlt = float(input("Bezahlt: "))
     rueckgeld = bezahlt - preis
     print("Das Rückgeld beträgt: ", rueckgeld)
-    weiter = input("Weitere Eingabe? j=ja  ")
+    weiter = input("Weitere Eingabe?  ")
 print("Kasse geschlossen")
