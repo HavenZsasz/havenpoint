24ea4e746587b5222610dda99549704cb21e7e0a
iPsXwhite <timko999@icloud.com>
2022-05-27 16:49:25 +0200

    Update funktionen.py

--- backend/funktionen.py
+++ backend/funktionen.py
@@ -11,5 +11,15 @@ def print_some_stars(lv):
     while eingabe > lv:
         print("*", end = "")
         lv += 1
+    print("\n")    
 
 print_some_stars(5)
+
+def print_with_paramters(zeichen, anzahl, inhalt):
+ 
+
+    print(zeichen * anzahl)
+    print(inhalt)
+    print(zeichen * anzahl)
+
+print_with_paramters(input("Zeichen angeben: "), int(input("Anzahl der Zeichen: ")), input("Inhalt angeben: "))
