621fa205afc8bcf7003e89b69c303b97f2917dc8
iPsXwhite <timko999@icloud.com>
2022-05-31 14:14:55 +0200

    update mobile version

--- prototypes/frontpage_JS/dropdown.js
+++ prototypes/frontpage_JS/dropdown.js
@@ -1,19 +1,38 @@
 function setup_dropdown_mehr() {
 
     document.getElementById("mehr").addEventListener("click", dropdown_mehr);
+    document.getElementById("close-mobile-dropdown").addEventListener("click", dropdown_close);
 
 }
 
 function dropdown_mehr() {
 
+    var inserate = document.getElementById("inserate");
+    var mehr = document.getElementById("mehr");
+    mehr.style.display = "none";
+    inserate.style.display = "none";
     var dropdownDiv = document.getElementById("dropdown-mehr");
     var wholeSite = document.getElementById("all");
     var helper1 = document.getElementById("helper");
     helper1.innerHTML = "HIDDEN";
     dropdownDiv.style.display = "block";
-    
+    wholeSite.style.marginRight = "55%";
 
 
+
+}
+
+function dropdown_close() {
+    var inserate = document.getElementById("inserate");
+    var mehr = document.getElementById("mehr");
+    mehr.style.display = "block";
+    inserate.style.display = "block";
+    var dropdownDiv = document.getElementById("dropdown-mehr");
+    var wholeSite = document.getElementById("all");
+    var helper1 = document.getElementById("helper");
+    helper1.innerHTML = "HIDDEN";
+    dropdownDiv.style.display = "none";
+    wholeSite.style.marginRight = "0%";
 }
 
 window.addEventListener("load", setup_dropdown_mehr);
