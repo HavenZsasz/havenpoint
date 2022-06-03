906310c946f15cca6fbcc89d781ea0194ba5349a
iPsXwhite <timko999@icloud.com>
2022-06-03 16:43:33 +0200

    update darkmode

--- prototypes/frontpage_JS/darkmode.js
+++ prototypes/frontpage_JS/darkmode.js
@@ -1,15 +1,83 @@
 function setup() {
-    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    
+    document.getElementById("lightmode").addEventListener("click", toggleDarkmode);
+    document.getElementById("darkmode").addEventListener("click", toggleDarkmode);
+    document.getElementById("auto-dark-light").addEventListener("click", toggleDarkmode);
 
-    if (prefersDarkScheme.matches) {
-        document.body.classList.add("dark-theme");
-        logo.setAttribute("src", "logoNightKomprimiert1.png");
+    if (localStorage.getItem("counterDarkmodeHavn") == undefined) {
 
+        if (prefersDarkScheme.matches) {
+            document.body.classList.add("dark-theme");
+            logo.setAttribute("src", "logoNightKomprimiert1.png");
+
+        } else {
+            document.body.classList.remove("dark-theme");
+            logo.setAttribute("src", "logoDayKomprimiert1.png");
+        }
     } else {
-        document.body.classList.remove("dark-theme");
-        logo.setAttribute("src", "logoDayKomprimiert1.png");
+
+        counter = JSON.parse(localStorage["counterDarkmodeHavn"]);
+        if (counterDarkmodeHavn == 0) {
+            document.body.classList.remove("dark-theme");
+            logo.setAttribute("src", "logoDayKomprimiert1.png");
+        } else if (counterDarkmodeHavn == 1) {
+            document.body.classList.add("dark-theme");
+            logo.setAttribute("src", "logoNightKomprimiert1.png");
+        } else {
+
+            if (prefersDarkScheme.matches) {
+                document.body.classList.add("dark-theme");
+                logo.setAttribute("src", "logoNightKomprimiert1.png");
+
+            } else {
+                document.body.classList.remove("dark-theme");
+                logo.setAttribute("src", "logoDayKomprimiert1.png");
+            }
+
+        }
+
     }
 }
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+var counterDarkmodeHavn;
+
+function toggleDarkmode(event) {
+    if (localStorage.getItem("counterDarkmodeHavn") == undefined) {
+        var counterDarkmodeHavn = 0;
+        localStorage["ccounterDarkmodeHavn"] = JSON.stringify(counterDarkmodeHavn);
+    }
+
+    if (event.target == document.getElementById("lightmode")) {
+        counterDarkmodeHavn = 0;
+        localStorage["ccounterDarkmodeHavn"] = JSON.stringify(counterDarkmodeHavn);
+        document.body.classList.remove("dark-theme");
+        logo.setAttribute("src", "logoDayKomprimiert1.png");
+    } else if(event.target == document.getElementById("darkmode")) {
+        counterDarkmodeHavn = 1;
+        localStorage["counterDarkmodeHavn"] = JSON.stringify(counterDarkmodeHavn);
+        document.body.classList.add("dark-theme");
+        logo.setAttribute("src", "logoNightKomprimiert1.png");
+    } else {
+
+        if (prefersDarkScheme.matches) {
+            document.body.classList.add("dark-theme");
+            logo.setAttribute("src", "logoNightKomprimiert1.png");
+
+        } else {
+            document.body.classList.remove("dark-theme");
+            logo.setAttribute("src", "logoDayKomprimiert1.png");
+        }
+
+    }
+
+
+
+
+
+}
+
+
 
 
 window.addEventListener("load", setup);
