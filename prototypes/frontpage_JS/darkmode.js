6f71e1a9617549fcadcdcd46e24325c5156ffde0
iPsXwhite <timko999@icloud.com>
2022-06-03 17:40:46 +0200

    update darkmode buttons

--- prototypes/frontpage_JS/darkmode.js
+++ prototypes/frontpage_JS/darkmode.js
@@ -21,18 +21,28 @@ function setup() {
         if (counterDarkmodeHavn == 0) {
             document.body.classList.remove("dark-theme");
             logo.setAttribute("src", "logoDayKomprimiert1.png");
+            document.getElementById("lightmode").style.backgroundColor = "black";
+            document.getElementById("lightmode").style.color = "white";
         } else if (counterDarkmodeHavn == 1) {
             document.body.classList.add("dark-theme");
             logo.setAttribute("src", "logoNightKomprimiert1.png");
+            document.getElementById("darkmode").style.backgroundColor = "rgb(0, 220, 253)";
+            document.getElementById("darkmode").style.color = "#121212";
+        
         } else {
 
             if (prefersDarkScheme.matches) {
                 document.body.classList.add("dark-theme");
                 logo.setAttribute("src", "logoNightKomprimiert1.png");
+                document.getElementById("auto-dark-light").style.backgroundColor = "rgb(0, 220, 253)";
+                document.getElementById("auto-dark-light").style.color = "#121212";
 
             } else {
                 document.body.classList.remove("dark-theme");
                 logo.setAttribute("src", "logoDayKomprimiert1.png");
+                document.getElementById("auto-dark-light").style.backgroundColor = "black";
+                document.getElementById("auto-dark-light").style.color = "rgb(0, 220, 253";
+
             }
 
         }
@@ -52,21 +62,46 @@ function toggleDarkmode(event) {
     if (event.target == document.getElementById("lightmode")) {
         localStorage.setItem('counterDarkmodeHavn', '0');
         document.body.classList.remove("dark-theme");
+        document.getElementById("lightmode").style.backgroundColor = "black";
+        document.getElementById("lightmode").style.color = "white";
+        document.getElementById("darkmode").style.backgroundColor = "#bbb";
+        document.getElementById("darkmode").style.color = "black";
+        document.getElementById("auto-dark-light").style.backgroundColor = "#bbb";
+        document.getElementById("auto-dark-light").style.color = "black";
         logo.setAttribute("src", "logoDayKomprimiert1.png");
     } else if(event.target == document.getElementById("darkmode")) {
         localStorage.setItem('counterDarkmodeHavn', '1');
         document.body.classList.add("dark-theme");
         logo.setAttribute("src", "logoNightKomprimiert1.png");
+        document.getElementById("darkmode").style.backgroundColor = "rgb(0, 220, 253)";
+        document.getElementById("darkmode").style.color = "black";
+        document.getElementById("lightmode").style.backgroundColor = "black";
+        document.getElementById("lightmode").style.color = "rgb(228, 221, 221)";
+        document.getElementById("auto-dark-light").style.backgroundColor = "#121212";
+        document.getElementById("auto-dark-light").style.color = "rgb(228, 221, 221)";
+
     } else {
         localStorage.setItem('counterDarkmodeHavn', '3');
 
         if (prefersDarkScheme.matches) {
             document.body.classList.add("dark-theme");
             logo.setAttribute("src", "logoNightKomprimiert1.png");
+            document.getElementById("auto-dark-light").style.backgroundColor = "rgb(0, 220, 253)";
+            document.getElementById("auto-dark-light").style.color = "black";
+            document.getElementById("darkmode").style.backgroundColor = "rgb(0, 220, 253)";
+            document.getElementById("darkmode").style.color = "black";
+            document.getElementById("lightmode").style.backgroundColor = "black";
+            document.getElementById("lightmode").style.color = "rgb(228, 221, 221)";
 
         } else {
             document.body.classList.remove("dark-theme");
             logo.setAttribute("src", "logoDayKomprimiert1.png");
+            document.getElementById("auto-dark-light").style.backgroundColor = "black";
+            document.getElementById("auto-dark-light").style.color = "rgb(0, 220, 253)";
+            document.getElementById("lightmode").style.backgroundColor = "#bbb";
+            document.getElementById("lightmode").style.color = "black";
+            document.getElementById("darkmode").style.backgroundColor = "#bbb";
+            document.getElementById("darkmode").style.color = "black";
         }
 
     }
