18a3cedd21006527549e45d5e1c7e167a1b96198
iPsXwhite <timko999@icloud.com>
2022-06-07 21:09:55 +0200

    Create rekursion.py

---
+++ backend/rekursion.py
@@ -0,0 +1,10 @@
+def zahlenVerdreher(zahl):
+    if zahl > 5:
+        zahl = zahl - 1
+        print(zahl)
+        zahl = zahlenVerdreher(zahl)
+        return zahl
+    return zahl
+
+zahl = int(input())
+print(zahlenVerdreher(zahl))    
