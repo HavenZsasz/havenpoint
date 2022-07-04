9ec613911eccb48873fd87c7ee038a368613a1ba
iPsXwhite <timko999@icloud.com>
2022-07-04 21:05:29 +0200

    update plus dropdown

--- prototypes/frontpage_JS/plus_dropdown.js
+++ prototypes/frontpage_JS/plus_dropdown.js
@@ -7,6 +7,7 @@ function plusOpen() {
 
 
     document.getElementById("plus-container").style.animation = "rotate 1s forwards";
+    document.getElementById("plus-dropdown").style.display = "block";
 
     document.getElementById("plus-container").removeEventListener("click", plusOpen);
     document.getElementById("plus-container").addEventListener("click", plusClose);
@@ -18,6 +19,7 @@ function plusOpen() {
 function plusClose() {
 
     document.getElementById("plus-container").style.animation = "rotateBack 1s forwards";
+    document.getElementById("plus-dropdown").style.display = "none";
     document.getElementById("plus-container").removeEventListener("click", plusClose);
     document.getElementById("plus-container").addEventListener("click", plusOpen);
 
