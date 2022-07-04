396f90cb0f64175909947418c2d5c37001f5f990
iPsXwhite <timko999@icloud.com>
2022-07-04 18:29:21 +0200

    update plus

--- prototypes/frontpage_JS/plus_dropdown.js
+++ prototypes/frontpage_JS/plus_dropdown.js
@@ -1,13 +1,17 @@
 function setupPlus() {
     document.getElementById("plus-container").addEventListener("click", plusOpen);
+    document.getElementById("plus-container").addEventListener("touchstart", plusOpen);
+
 }
 
 function plusOpen() {    
+    document.getElementById("plus-container").style.animation = "rotate 1s forwards";
     document.getElementById("plus-container").removeEventListener("click", plusOpen);
     document.getElementById("plus-container").addEventListener("click", plusClose);
 }
 
 function plusClose() {
+
     document.getElementById("plus-container").removeEventListener("click", plusClose);
     document.getElementById("plus-container").addEventListener("click", plusOpen);
 }
