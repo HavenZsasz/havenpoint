cab1976a31c8591d9b0e72cda54f971b5f5bda03
iPsXwhite <timko999@icloud.com>
2022-07-05 17:46:26 +0200

    update

--- prototypes/frontpage_JS/dropdown.js
+++ prototypes/frontpage_JS/dropdown.js
@@ -13,7 +13,6 @@ function dropdown_mehr() {
     document.getElementById('all').style.animation="allSwipeLeft 0.5s forwards";
     
     document.getElementById("dropdown-mehr").style.animation= "dropDownOpen 1s forwards";
-    document.getElementById("entdecken").setAttribute("href", "");
     document.getElementById("neuware").setAttribute("href", "");
     document.getElementById("entdecken").style.pointerEvents = "none";
     document.getElementById("neuware").style.pointerEvents = "none";
@@ -37,7 +36,6 @@ function dropdown_close() {
     var wholeSite = document.getElementById("all");
     document.getElementById("dropdown-mehr").style.animation= "dropDownClose 0.5s forwards";
     wholeSite.style.animation = "allSwipeRight 1s forwards";
-    document.getElementById("entdecken").setAttribute("href", "entdecken.html");
     document.getElementById("entdecken").style.pointerEvents = "all";
     document.getElementById("plus-container").addEventListener("click", plusOpen);
 }
