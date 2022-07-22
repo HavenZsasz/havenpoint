c46a27413d34be4c00fc70184ace564bde484f01
HavenZsasz <silverhand@havenpoint.de>
2022-07-22 08:44:48 +0200

    Book

--- schroedi/JS/cookies.js
+++ schroedi/JS/cookies.js
@@ -3,7 +3,7 @@ window.addEventListener("load", setup);
 
 function setup () {
 
-    document.getElementById("wert").addEventListener("blur", writer);
+    document.getElementById("wert").addEventListener("blur", writer3);
 
 }
 
@@ -12,7 +12,8 @@ function writer(){
     var name = document.getElementById("name");
     var wert = document.getElementById("wert");
 
-    document.cookie = name.value + "=" + wert.value;
+    document.cookie = name.value + "=" + wert.value + "; expires=Tue, 21 Jul 2022 16:27:00 UTC";
+    //Um einen Cookie zu löschen, setzt man einen Cookie mit demselben Namen und einem Verfallsdatum in der Vergangenheit. Somit wird er gelöscht
 
     name.value = "";
     wert.value = "";
@@ -21,3 +22,109 @@ function writer(){
 
 
 }
+
+//Auf den localStorage zugreifen:
+
+
+//localStorage.setItem("Zucker", "2kg");
+//localStorage["Eier"] = 12; //Im localStorage werden nur Strings gespeichert, will man einen Integer muss das über parseInt() passieren
+// alert(localStorage.getItem("Eier"));
+// alert(localStorage["Zucker"]);
+
+//Es ist sinnvoll, erst zu checken, ob der localStorage überhaupt zur Verfügung steht:
+if(window.localStorage){
+
+}
+
+for(var i = 0; i < localStorage.length; i++) {
+
+    var name = localStorage.key(i);
+    var wert = localStorage[name];
+
+    //alert(name + " " + wert);
+
+}
+
+function writer2() {
+
+    var name1 = document.getElementById("name").value;
+    var wert1 = document.getElementById("wert").value;
+
+    localStorage.setItem(name1, wert1);
+    //oder: localStorage[name1] = wert1;
+
+    
+    var target = document.getElementById("zettel");
+
+    while(target.firstChild) {
+        target.removeChild(target.firstChild);
+    }
+    
+    for(var i = 0; i < localStorage.length; i++) {
+
+        var name2  = localStorage.key(i);
+        var wert2 = localStorage[name2];
+
+        
+
+        var node = document.createElement("li");
+
+        var text = document.createTextNode(name2 + ": " + wert2 + " ");
+        
+        node.appendChild(text);
+
+        target.appendChild(node);
+
+
+    }
+
+    name1 = "";
+    wert2 = "";
+
+
+}
+
+//Mit den folgenden Zeilen kann man statt eines Objekts auch einen String im localStorage speichern
+//bzw. auslesen:
+
+//var arr = [1,2,3,4,5];
+//var arr2 = [];
+//localStorage["arr"] = JSON.stringify(arr);
+//arr2 = JSON.parse(localStorage["arr"]);
+//console.log(arr2);
+//console.log(localStorage);
+
+
+
+
+
+
+
+function writer3() {
+
+    var name1 = document.getElementById("name").value;
+    var wert1 = document.getElementById("wert").value;
+    var arr = [];
+
+    var string = name1+", "+wert1;
+
+
+    
+    var target = document.getElementById("zettel");
+
+    while(target.firstChild) {
+        target.removeChild(target.firstChild);
+    }
+
+    
+
+    
+
+    name1 = "";
+    wert2 = "";
+
+
+}
+
+
+
