6dc7ce467edb268e54f8490d5c86bfaf125f09a9
HavenZsasz <silverhand@havenpoint.de>
2022-07-23 23:52:27 +0200

    Book

--- schroedi/JS/multimedia.js
+++ schroedi/JS/multimedia.js
@@ -3,6 +3,9 @@ function setup() {
     document.getElementById("mehr").addEventListener("click", () => {
         document.querySelector("audio").play();
     })
+    document.getElementById("click-logo").addEventListener("click", () => {
+        document.querySelector("audio").play();
+    })
     
 }
 
