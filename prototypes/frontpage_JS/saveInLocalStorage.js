4b2fc43ee0c8cf070bf65b45c11c3e1966a1e67c
iPsXwhite <timko999@icloud.com>
2022-08-10 17:28:03 +0200

    update

--- prototypes/frontpage_JS/saveInLocalStorage.js
+++ prototypes/frontpage_JS/saveInLocalStorage.js
@@ -1,6 +1,15 @@
 function loginSaveSetup() {
 
-    document.getElementById("start").addEventListener("click", loginSave);
+
+    if (localStorage.getItem("loginHavn")) {
+
+        window.open("https://htmlpreview.github.io/?https://raw.githubusercontent.com/HavenZsasz/havenpoint/main/prototypes/frontpage.html", "_self");
+
+    } else {
+
+        document.getElementById("start").addEventListener("click", loginSave);
+
+    }
 
 }
 
@@ -8,16 +17,24 @@ function loginSave() {
     var emailInput = document.getElementById("email").value;
     var passwortInput = document.getElementById("passwort").value;
 
-    if(emailInput == "admin" && passwortInput == "0") {
+
+
+    if (emailInput == "admin" && passwortInput == "0") {
         var loginHavn = "signedIn";
         localStorage.setItem('loginHavn', 'signedIn');
-        window.open("https://htmlpreview.github.io/?https://raw.githubusercontent.com/HavenZsasz/havenpoint/main/prototypes/frontpage.html","_self");
+        window.open("https://htmlpreview.github.io/?https://raw.githubusercontent.com/HavenZsasz/havenpoint/main/prototypes/frontpage.html", "_self");
 
     } else {
 
         window.alert("Anmeldedaten sind ungültig! Probiers mal mit 'admin' und '0'");
 
     }
+
+
+
+
+
+
 }
 
 window.addEventListener("load", loginSaveSetup);
