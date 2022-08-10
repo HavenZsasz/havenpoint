6b416ebbc11841e2d04ac07ad2945371b12fce09
iPsXwhite <timko999@icloud.com>
2022-08-10 21:10:54 +0200

    update

---
+++ prototypes/frontpage_JS/openSignIn.js
@@ -0,0 +1,9 @@
+function setupAnmelden() {
+    document.getElementById("anmelden").addEventListener("click", dropAnmelden);
+}
+
+function dropAnmelden() {
+    
+}
+
+window.addEventListener("load", setupAnmelden);
