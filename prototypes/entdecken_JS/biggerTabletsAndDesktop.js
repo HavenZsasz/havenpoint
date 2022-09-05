97c1ae583ad3367f4cbbf1c945395412681369e7
HavenZsasz <socials@havenpoint.de>
2022-09-05 15:45:20 +0200

    Entdecken_Tabs

--- prototypes/entdecken_JS/biggerTabletsAndDesktop.js
+++ prototypes/entdecken_JS/biggerTabletsAndDesktop.js
@@ -12,9 +12,18 @@ function setup() {
     document.getElementById("slideshow").addEventListener("mouseout", hideButtons);
     //#endregion
 
-    document.getElementById("neuware_tab").addEventListener("mouseover", erweiterung_aktivieren);
+    //#region Tabs Event-Listener
+
+    var tabs = document.getElementById("menu").getElementsByTagName("a");
+
+    for(var i = 1; i < tabs.length; i++) {
+        tabs[i].addEventListener("mouseover", erweiterung_aktivieren);
+    }
+
+    
     document.getElementById("tab_erweiterungen").addEventListener("mouseout", erweiterung_deaktivieren);
     
+    //#endregion
 }
 
 //#region /////////////////////// SLIDER ///////////////////////
@@ -83,17 +92,60 @@ function hideButtons() {
 
 //#region ############# TABS ##############
 
-function erweiterung_aktivieren() {
+
+function erweiterung_aktivieren(event) {
+
+    
+   
+    //Zuerst werden sämtliche sichtbaren tabs zurückgesetzt
+    var tabs = document.getElementById("tab_erweiterungen").getElementsByClassName("erweiterungen");
+
+    for(var i = 0; i < tabs.length; i++) {
+        
+        tabs[i].setAttribute("style", "display:none");
+        
+    }
+
+    //Diese Klasse schaut nun, welches Tab aktiviert werden soll und macht dieses sichtbar
+    erweiterung_aktivieren2(event);
+    
+    //dieser event-handler muss registriert werden, da sonst nicht der zugrundeliegende div mit dem Mouse-Over verknüpft wird
+    document.getElementById("tab_erweiterungen").addEventListener("mouseover", erweiterung_aktivieren2);
+
+
+    
+}
+
+function erweiterung_aktivieren2(event) {
 
     document.getElementById("tab_erweiterungen").setAttribute("style", "display:block");
 
-    document.getElementById("tab_erweiterungen").addEventListener("mouseover", erweiterung_aktivieren);
+    if(event.target.id == "neuware_tab") {
+        document.getElementById("neuware_erweiterung").setAttribute("style", "display:flex");
+    }
+    if(event.target.id == "inserate_tab") {
+        document.getElementById("inserate_erweiterung").setAttribute("style", "display:flex");
+    }
+    if(event.target.id == "dienstleistungen_tab") {
+        document.getElementById("dienstleistungen_erweiterung").setAttribute("style", "display:flex");
+    }
+    if(event.target.id == "automobil_tab") {
+        document.getElementById("automobil_erweiterung").setAttribute("style", "display:flex");
+    }
+    if(event.target.id == "immobilien_tab") {
+        document.getElementById("immobilien_erweiterung").setAttribute("style", "display:flex");
+    }
+
 }
 
 function erweiterung_deaktivieren() {
 
     document.getElementById("tab_erweiterungen").setAttribute("style", "display:none");
 
-}
+
+    
+    
+
+} 
 
 //#endregion
