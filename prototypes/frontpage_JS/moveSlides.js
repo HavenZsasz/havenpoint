c7d5eed39e37c0d6a233cfbf45627f6d4bceca9c
iPsXwhite <timko999@icloud.com>
2022-09-05 17:59:57 +0200

    update

---
+++ prototypes/frontpage_JS/moveSlides.js
@@ -0,0 +1,35 @@
+function slides() {
+    document.getElementsByClassName("dot")[0].addEventListener("click", currentSlide(1));
+    document.getElementsByClassName("dot")[1].addEventListener("click", currentSlide(2));
+    document.getElementsByClassName("dot")[2].addEventListener("click", currentSlide(3));
+
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+window.addEventListener("load", slides);
--- prototypes/frontpage_JS/moveSlidesAuto.js
+++
@@ -1,5 +0,0 @@
-function slides() {
-
-}
-
-window.addEventListener("load", slides);
