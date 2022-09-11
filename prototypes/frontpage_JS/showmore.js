97cddfbcf9a1afabe2ec680917554a152ec0d8b6
iPsXwhite <timko999@icloud.com>
2022-09-11 22:08:10 +0200

    update

--- prototypes/frontpage_JS/showmore.js
+++ prototypes/frontpage_JS/showmore.js
@@ -1,10 +1,19 @@
 function setupShowMore() {
-    document.getElementById("mehr-anzeigen").addEventListener("click", showMoreInteresting);
+    document.getElementById("mehr-anzeigen").addEventListener("click", showMore);
+    document.getElementById("mehr-anzeigen-2").addEventListener("click", showMore);
 }
 
-function showMoreInteresting() {
-    document.getElementById("mehr-anzeigen").style.display = "none";
-    document.getElementById("alle-vorschlaege-container-interessant-2").style.display = "flex";
+function showMore(event) {
+    if(event.target == document.getElementById("mehr-anzeigen")) {
+        document.getElementById("flex-for-mehr-anzeigen").style.display = "none";
+        document.getElementById("alle-vorschlaege-container-interessant-2").style.display = "flex";
+
+    } else if(event.target == document.getElementById("mehr-anzeigen-2")) {
+        document.getElementById("flex-for-mehr-anzeigen-2").style.display = "none";
+        document.getElementById("alle-vorschlaege-container-trend-2").style.display = "flex";
+
+    }
+    
 
 }
 
