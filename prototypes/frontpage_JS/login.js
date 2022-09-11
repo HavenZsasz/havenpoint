e4b1b838974fe2b6990089880c3457cbb80538b2
iPsXwhite <timko999@icloud.com>
2022-09-12 00:45:00 +0200

    update

--- prototypes/frontpage_JS/login.js
+++ prototypes/frontpage_JS/login.js
@@ -25,15 +25,32 @@ function loginSave() {
 
 
     if (emailInput == "admin" && passwortInput == "0") {
+        document.getElementById("logindaten-falsch").style.display = "none";
         var loginHavn = "signedIn";
         localStorage.setItem('loginHavn', 'signedIn');
         document.getElementById("logout").style.display ="block";
         document.location.reload();
+        document.getElementById("login-mail").style.backgroundColor = "rgb(120,120,120)";
+        document.getElementById("login-passwort").style.backgroundColor = "rgb(120,120,120)";
         
 
     } else {
 
-        window.alert("Anmeldedaten sind ungültig!");
+
+        if(passwortInput != 0 && emailInput == "admin") {
+            document.getElementById("login-mail").style.backgroundColor = "rgb(120,120,120)";
+            document.getElementById("login-passwort").style.backgroundColor = "red";
+            document.getElementById("logindaten-falsch").style.display = "block";
+            document.getElementById("logindaten-falsch").innerHTML = "Das Passwort ist falsch!";
+
+        } else {
+            document.getElementById("login-mail").style.backgroundColor = "red";
+            document.getElementById("login-passwort").style.backgroundColor = "red";
+            document.getElementById("logindaten-falsch").style.display = "block";
+            document.getElementById("logindaten-falsch").innerHTML = "Die E-Mail Adresse bzw. das dazugehörige Passwort ist falsch!";
+
+        }
+       
 
     }
 }
