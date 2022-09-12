93c656fe3d418b5a58f56dcb023f06756591ab38
iPsXwhite <timko999@icloud.com>
2022-09-12 13:31:12 +0200

    Update plus_dropdown.js

--- prototypes/frontpage_JS/plus_dropdown.js
+++ prototypes/frontpage_JS/plus_dropdown.js
@@ -6,7 +6,7 @@ function plusOpen() {
 
 
     document.getElementById("plus-container").style.animation = "rotate 0.5s forwards";
-
+    document.getElementById("plus-container").style.border = "0";
     document.getElementById("plus-container").removeEventListener("click", plusOpen);
     document.getElementById("plus-container").addEventListener("click", plusClose);
 
@@ -26,6 +26,22 @@ function plusClose() {
     document.getElementById("plus-container").style.animation = "rotateBack 1s forwards";
     document.getElementById("plus-dropdown").style.display = "none";
     document.getElementById("seiteninhalt").style.marginTop = "0px";
+    if(localStorage.getItem("counterDarkmodeHavn") == undefined || localStorage.getItem("counterDarkmodeHavn") == 0) {
+        document.getElementById("plus-container").style.border = "1px solid rgb(51, 51, 51)";
+    } else {
+        if(localStorage.getItem("counterDarkmodeHavn") == 3) {
+            if(prefersDarkScheme.matches) {
+                document.getElementById("plus-container").style.border = "1px solid rgba(221, 216, 216, 0.558)";
+
+            } else {
+                document.getElementById("plus-container").style.border = "1px solid rgb(51, 51, 51)";
+
+            }
+        } else {
+            document.getElementById("plus-container").style.border = "1px solid rgba(221, 216, 216, 0.558)";
+
+        }
+    }
     document.getElementById("plus-container").removeEventListener("click", plusClose);
     document.getElementById("plus-container").addEventListener("click", plusOpen);
 
