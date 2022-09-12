170f170a2db8d37e194c64c1ba2b79b7a76e912b
iPsXwhite <timko999@icloud.com>
2022-09-12 23:52:41 +0200

    udate

--- prototypes/frontpage_JS/loginCheck.js
+++ prototypes/frontpage_JS/loginCheck.js
@@ -3,7 +3,6 @@ function loginCheck() {
         document.getElementById("login-inhalt").style.display = "none";
         document.getElementById("round-container-mobile").style.display = "none";
         document.getElementById("anmelden").style.display = "inline";
-        document.getElementById("plus-container").style.display = "none";
     } else if(localStorage.getItem("loginHavn")) {
         document.getElementById("anmelden").style.display = "none";
         document.getElementById("login-inhalt").style.display = "block";
