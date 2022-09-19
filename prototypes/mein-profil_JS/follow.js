fbbd87657afa47350d794041c14e84a3d0214aa1
iPsXwhite <timko999@icloud.com>
2022-09-19 23:54:11 +0200

    update

--- prototypes/mein-profil_JS/follow.js
+++ prototypes/mein-profil_JS/follow.js
@@ -1,5 +1,27 @@
 function follow_setup__m() {
     document.getElementById("follow-profile_m").addEventListener("click", follow_m);
+    if(!localStorage.getItem("followMmHavn")) {
+        document.getElementById("follow-profile_m").innerHTML = "Folgen";
+    } else {
+        document.getElementById("follow-profile_m").innerHTML = "Gefolgt";
+
+    }
+}
+
+function follow_m() {
+
+    if(!localStorage.getItem("followMmHavn")) {
+        document.getElementById("follow-profile_m").innerHTML = "Gefolgt";
+        var followMmhavn = true;
+        localStorage.setItem('followMmHavn',true);
+    } else {
+        document.getElementById("follow-profile_m").innerHTML = "Folgen";
+        followMmhavn = false;
+        localStorage.removeItem('followMmHavn',false);
+
+    }
+
+
 }
 
 window.addEventListener("load", follow_setup__m);
