d1db7af6f6a735750462ff0d2cf3d5384a748175
iPsXwhite <timko999@icloud.com>
2022-09-21 00:17:34 +0200

    update

--- prototypes/profiloverview_JS/openSearchBar_m.js
+++ prototypes/profiloverview_JS/openSearchBar_m.js
@@ -3,12 +3,16 @@ function setupSearchBar_m() {
 }
 
 function openSearchbar_m() {
+    document.getElementById("suchlupe_m").removeEventListener("click", openSearchbar_m);
     document.getElementById('over-div-suchfeld_m').style.display="flex";
+    document.getElementById("suchfeld_m").style.animation = "openSearchbar 0.5s forwards";
     document.getElementById("suchlupe_m").addEventListener("click", closeSearchbar_m);
 
 }
 
 function closeSearchbar_m() {
+    document.getElementById("suchlupe_m").removeEventListener("click", closeSearchbar_m);
+    document.getElementById("suchfeld_m").style.animation = "closeSearchbar 0.2s forwards";
     document.getElementById('over-div-suchfeld_m').style.display="none";
     document.getElementById("suchlupe_m").addEventListener("click", openSearchbar_m);
 
