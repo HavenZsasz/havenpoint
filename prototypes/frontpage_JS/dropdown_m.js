4ec29f037b78d78cbb245fc6514d6583663d4ed4
iPsXwhite <timko999@icloud.com>
2022-09-25 01:22:00 +0200

    update

--- prototypes/frontpage_JS/dropdown_m.js
+++ prototypes/frontpage_JS/dropdown_m.js
@@ -4,9 +4,8 @@ function dropdown_mehr_m() {
     document.getElementById('all_m').style.animation="allSwipeLeft 0.5s forwards";
     
     document.getElementById("dropdown-mehr_m").style.animation= "dropDownOpen 1s forwards";
-    document.getElementById("neuware_m").setAttribute("href", "");
     document.getElementById("entdecken_m").style.pointerEvents = "none";
-    document.getElementById("neuware_m").style.pointerEvents = "none";
+    document.getElementById("frontpage_m").style.pointerEvents = "none";
     document.getElementById("click-logo_m").style.pointerEvents = "none";
     document.getElementById("plus-container_m").removeEventListener("click", plusOpen_m);
     
@@ -24,6 +23,7 @@ function dropdown_close_m() {
     document.getElementById("dropdown-mehr_m").style.animation= "dropDownClose 0.5s forwards";
     wholeSite_m.style.animation = "allSwipeRight 1s forwards";
     document.getElementById("entdecken_m").style.pointerEvents = "all";
+    document.getElementById("frontpage_m").style.pointerEvents = "all";
     document.getElementById("click-logo_m").style.pointerEvents = "all";
     document.getElementById("plus-container_m").addEventListener("click", plusOpen_m);
 }
