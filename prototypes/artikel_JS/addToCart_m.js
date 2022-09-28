5ca6f5895b84a57ebde9cff040c688321411aee2
iPsXwhite <timko999@icloud.com>
2022-09-29 00:17:47 +0200

    Update addToCart_m.js

--- prototypes/artikel_JS/addToCart_m.js
+++ prototypes/artikel_JS/addToCart_m.js
@@ -2,26 +2,23 @@ function setupaddToCart_m() {
     document.getElementById("in-den-warenkorb_m").addEventListener("click", addTocard_m);
 
 }
-var cartcounter_m = 0;
+var cartcounter_m;
 
 function addTocard_m(event) {
-    cartcounter_m += 1;
+    
     if(!localStorage.getItem("cartUsed")) {
         var cartUsed;
         localStorage.setItem("cartUsed", 1);
     }
 
-    cartcounter_m = localStorage.getItem("cartUsed");
-    cartcounter_m = parseInt(cartcounter_m + 1);
-    let stringer = cartcounter_m.toString();
+    localStorage.setItem("cartUsed", ++cartUsed);
+    document.getElementById("cart-counter_m").innerHTML = localStorage.getItem("cartUsed");
 
-    document.getElementById("cart-counter_m").innerHTML = stringer;
-    localStorage.setItem("cartUsed", stringer);
 
     
     
     if(event.target == document.getElementById("in-den-warenkorb_m")) {
-        if(localStorage.getItem("cartused"))
+       
         document.getElementById("div-cart_m").style.display = "flex";
     }
 
