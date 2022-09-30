7151676b7472412de9af204d2caa72e18b7182b7
iPsXwhite <timko999@icloud.com>
2022-09-30 23:41:03 +0200

    update

--- prototypes/artikel_JS/addToCart_m.js
+++ prototypes/artikel_JS/addToCart_m.js
@@ -2,17 +2,29 @@ function setupaddToCart_m() {
     document.getElementById("in-den-warenkorb_m").addEventListener("click", addTocard_m);
 
 }
-var cartcounter_m;
+
+var cartUsed;
+
 
 function addTocard_m(event) {
-    
-    if(!localStorage.getItem("cartUsed")) {
-        var cartUsed;
-        localStorage.setItem("cartUsed", 1);
+
+    cartUsed = parseInt(localStorage.getItem("cartUsed")) + 1;
+    if(cartUsed > 9) {
+        document.getElementById("cart-counter_m").style.width = "20px";
+
     }
 
-    localStorage.setItem("cartUsed", ++cartUsed);
-    document.getElementById("cart-counter_m").innerHTML = localStorage.getItem("cartUsed");
+    document.getElementById("cart-counter_m").innerHTML = cartUsed;
+    document.getElementById("cart-counter_m").style.justifyContent = "center";
+    document.getElementById("cart-counter_m").style.alignItems = "center";
+    document.getElementById("cart-counter_m").style.paddingLeft = "2px";
+
+    localStorage.setItem("cartUsed", JSON.stringify(cartUsed));
+
+
+
+    
+
 
 
     
--- prototypes/artikel_JS/cartCheck_m.js
+++ prototypes/artikel_JS/cartCheck_m.js
@@ -1,10 +1,30 @@
 
 function setupCartCheck_m() {
-    if(localStorage.getItem("cartUsed")) {
+
+    if(localStorage.getItem("cartUsed") > 0) {
         document.getElementById("div-cart_m").style.display = "flex";
+        cartUsed = parseInt(localStorage.getItem("cartUsed")) +1;
+    if(cartUsed > 9) {
+        document.getElementById("cart-counter_m").style.width = "20px";
+
     }
+    cartUsed = parseInt(localStorage.getItem("cartUsed"))
+
+    document.getElementById("cart-counter_m").innerHTML = cartUsed;
+    document.getElementById("cart-counter_m").style.justifyContent = "center";
+    document.getElementById("cart-counter_m").style.alignItems = "center";
+    document.getElementById("cart-counter_m").style.paddingLeft = "2px";
+
+    localStorage.setItem("cartUsed", JSON.stringify(cartUsed));
+
+
+    } else if(!localStorage.getItem("cartUsed") || localStorage.getItem("cartUsed") < 1) {
+        document.getElementById("div-cart_m").style.display = "none";
+    }
+
 
     
 }
+
 
 window.addEventListener("load", setupCartCheck_m);
