50b3e38544e51ed330439c6780907a453102983a
iPsXwhite <timko999@icloud.com>
2022-09-30 23:42:46 +0200

    Update cartCheck_m.js

--- prototypes/artikel_JS/cartCheck_m.js
+++ prototypes/artikel_JS/cartCheck_m.js
@@ -1,14 +1,15 @@
 
 function setupCartCheck_m() {
 
-    if(localStorage.getItem("cartUsed") > 0) {
+    cartUsed = parseInt(localStorage.getItem("cartUsed"));
+
+    if(cartUsed > 0) {
         document.getElementById("div-cart_m").style.display = "flex";
-        cartUsed = parseInt(localStorage.getItem("cartUsed")) +1;
     if(cartUsed > 9) {
         document.getElementById("cart-counter_m").style.width = "20px";
 
     }
-    cartUsed = parseInt(localStorage.getItem("cartUsed"))
+    
 
     document.getElementById("cart-counter_m").innerHTML = cartUsed;
     document.getElementById("cart-counter_m").style.justifyContent = "center";
