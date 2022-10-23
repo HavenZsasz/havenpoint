5262e57fc9b2aa929169f92c6da54dbd3f75625f
HavenZsasz <socials@havenpoint.de>
2022-10-23 22:18:19 +0200

    WarenKorbBerechnung_d

--- prototypes/warenkorb_JS/kasseBerechnung_d.js
+++ prototypes/warenkorb_JS/kasseBerechnung_d.js
@@ -12,7 +12,7 @@ function calculateSum_d () {
     var artikel1 = parseInt(localStorage.getItem("artikel1"));
     var artikel1Preis = document.getElementById("artikelPreis_d").innerHTML;
 
-    var splitArray = artikel1Preis.split(/,| /);
+    var splitArray = artikel1Preis.split(/,|\.| /);
 
     console.log(splitArray);
 
@@ -21,13 +21,35 @@ function calculateSum_d () {
 
     var erg;
 
+    var thousands = splitArray[0];
+    var hundreds = splitArray[1]; 
+    var decimals = splitArray[2];
+
     if(localStorage.getItem("artikel1") || localStorage.getItem("artikel1") > 0) {
-        sum = artikel1*artikel1Preis;
+
+        decimals = decimals * artikel1;
+        hundreds = hundreds * artikel1;
+        thousands = thousands * artikel1;
+
+        while (decimals > 100) {
+            console.log(decimals);
+            decimals = decimals - 100;
+            hundreds = hundreds + 1;
+        }
+
+        while (hundreds > 1000) {
+            console.log(hundreds);
+            hundreds = hundreds - 1000;
+            thousands = thousands + 1;
+        }
+
     }
 
+    erg = thousands + "." + hundreds + "," + decimals + " EUR";
 
 
 
+    document.getElementById("summeRechnung_d").innerHTML = erg;
     document.getElementById("gesamtRechnung_d").innerHTML = erg;
 
 
