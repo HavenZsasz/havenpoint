c1a349788f5c313da8b5e7e25b6a79a8158f8a8d
HavenZsasz <socials@havenpoint.de>
2022-10-27 22:32:58 +0200

    MessagesPreview_d

--- prototypes/multiple_JS/openMessagePreview_d.js
+++ prototypes/multiple_JS/openMessagePreview_d.js
@@ -5,8 +5,11 @@ window.addEventListener("load", setup_d);
 
 function setup_d () {
 
-    document.getElementById("newMessage_d").addEventListener("click", openMessagePreview);
-    document.getElementById("chatContainer_d").addEventListener("click", openMessagePreview);
+    document.getElementById("newMessage_d").addEventListener("click", openMessagePreview_d);
+    document.getElementById("chatContainer_d").addEventListener("click", openMessagePreview_d);
+
+    document.getElementById("haendlerKnopf_d").addEventListener("click", openHaendlerMessage_d);
+    document.getElementById("userKnopf_d").addEventListener("click", openUserMessage_d);
     
 
 
@@ -17,17 +20,17 @@ function setup_d () {
 
 
 
-function openMessagePreview () {
+function openMessagePreview_d () {
 
     document.getElementById("messagePreviewContainer_d").setAttribute("style", "right:0;");
-    document.getElementById("closeMessagePreview_d").addEventListener("click", closeMessagePreview);
+    document.getElementById("closeMessagePreview_d").addEventListener("click", closeMessagePreview_d);
 
 
 }
 
-function closeMessagePreview ()  {
+function closeMessagePreview_d ()  {
     document.getElementById("messagePreviewContainer_d").setAttribute("style", "right:-33%;");
-    document.getElementById("closeMessagePreview_d").removeEventListener("click", closeMessagePreview);
+    document.getElementById("closeMessagePreview_d").removeEventListener("click", closeMessagePreview_d);
 }
 
 
@@ -35,6 +38,32 @@ function closeMessagePreview ()  {
 
 
 
+function openHaendlerMessage_d () {
+
+    document.getElementById("messagesInPreviewUser_d").setAttribute("style", "display:none;");
+    document.getElementById("messagesInPreviewHaendler_d").setAttribute("style","display:flex;");
+    
+    document.getElementById("haendlerKnopf_d").setAttribute("style","border-bottom:2px solid rgb(0,220,253);");
+    document.getElementById("userKnopf_d").setAttribute("style","border-bottom:none;");
+
+
+
+}
+
+
+function openUserMessage_d () {
+
+
+    document.getElementById("messagesInPreviewHaendler_d").setAttribute("style", "display:none;");
+    document.getElementById("messagesInPreviewUser_d").setAttribute("style","display:flex;");
+    
+    document.getElementById("userKnopf_d").setAttribute("style","border-bottom:2px solid rgb(0,220,253);");
+    document.getElementById("haendlerKnopf_d").setAttribute("style","border-bottom:none;");
+
+
+
+
+}
 
 
 
