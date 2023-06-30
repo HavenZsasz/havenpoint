48c7b8d6ea8e9b69eb31dc1424a6c6a9d356d119
iPsXwhite <100875419+iPsXwhite@users.noreply.github.com>
2023-06-30 14:43:44 +0200

    Delete backend directory

--- backend/datentypen.py
+++
@@ -1,1 +0,0 @@
-print(type(42), type(42.131), type(True), type("Hallo"))
--- backend/locale.py
+++
@@ -1,8 +0,0 @@
-import locale
-locale.setlocale(locale.LC_ALL, "de_DE")
-preis = input("Gib den Preis ein: ")
-zahlung = input("Der Kunde zahlt: ")
-preis = locale.atof(preis)
-zahlung =  locale.atof(zahlung)
-rueckgeld = zahlung - preis
-print("Wechselgeld: ", locale.format_string("%.2f", rueckgeld))
