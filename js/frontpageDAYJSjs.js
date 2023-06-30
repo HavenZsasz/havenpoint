149a0b38ba9832d3b32753f680c9194d41b32796
iPsXwhite <100875419+iPsXwhite@users.noreply.github.com>
2023-06-30 14:44:18 +0200

    Delete js directory

--- js/frontpageDAYJSjs.js
+++
@@ -1,9 +0,0 @@
-function Quadrat() {
-    var eingabe  = document.getElementById('eingabe');
-    var Ergebnis = eingabe.value * eingabe.value;
-    alert("Das Quadrat von " + eingabe.value + " = " + Ergebnis);
-    eingabe.value = 0;
-   }
-  
-  var los  = document.getElementById('calculate');
-  los.addEventListener ('click', Quadrat, true);
